c881df5c2ca664a455de8779f76212d835abe0b7
Zhong Ruoyu <zhongruoyu@outlook.com>
2021-12-21 22:31:32 +0800

    Update example

---
+++ test/example.cpp
@@ -0,0 +1,29 @@
+#include "thread_pool.h"
+
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+int main() {
+    ThreadPool pool;
+    unsigned n = pool.workers_count();
+    std::cout << n << " worker(s) in the pool" << std::endl;
+
+    std::vector<std::future<unsigned>> results;
+    for (unsigned i = 0; i != 2 * n; ++i) {
+        results.push_back(pool.Add([i] {
+            std::stringstream stream;
+            stream << "job " << i << " handled by worker "
+                   << std::this_thread::get_id() << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << stream.str();
+            return i;
+        }));
+    }
+
+    for (auto &&result : results) {
+        std::cout << result.get() << " ";
+    }
+    std::cout << std::endl;
+}
